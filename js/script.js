2ce326da3c540a270127357ebf7c2459f27e2d4b
mylesw27 <myleswiegel27@gmail.com>
2023-03-17 20:36:08 -0700

    Map keys and introduce game loop

--- js/script.js
+++ js/script.js
@@ -10,33 +10,34 @@ const ctx = canvas.getContext("2d")
 canvas.setAttribute('height', getComputedStyle(canvas).height)
 canvas.setAttribute('width', getComputedStyle(canvas).width)
 
-// Create End Zones
-ctx.fillStyle = "blue"
-ctx.fillRect(10, 10, 100, 480)
-ctx.fillStyle = "red"
-ctx.fillRect(1090, 10, 100, 480)
+let renderField = function () {
+    // Create End Zones
+    ctx.fillStyle = "blue"
+    ctx.fillRect(10, 10, 100, 480)
+    ctx.fillStyle = "red"
+    ctx.fillRect(1090, 10, 100, 480)
 
 
-// Create Side Lines
-ctx.fillStyle = "white"
-ctx.fillRect(10, 10, 1180, 10)
-ctx.fillRect(10, 480, 1180, 10)
-ctx.fillRect(0, 10, 10, 480)
-ctx.fillRect(1190, 10, 10, 480)
+    // Create Side Lines
+    ctx.fillStyle = "white"
+    ctx.fillRect(10, 10, 1180, 10)
+    ctx.fillRect(10, 480, 1180, 10)
+    ctx.fillRect(0, 10, 10, 480)
+    ctx.fillRect(1190, 10, 10, 480)
 
-// Create Yardage Lines (Current field is 980 px wide by 460px tall, each line marked 98 pixels apart)
-ctx.fillRect (110, 20, 1, 460) // goal line - Left End Zone
-ctx.fillRect (208, 20, 1, 460) // 10 Yard Line
-ctx.fillRect (306, 20, 1, 460) // 20 Yard Line
-ctx.fillRect (404, 20, 1, 460) // 30 Yard Line
-ctx.fillRect (502, 20, 1, 460) // 40 Yard Line
-ctx.fillRect (600, 20, 1, 460) // 50 Yard Line
-ctx.fillRect (698, 20, 1, 460) // opp 40 Yard Line
-ctx.fillRect (796, 20, 1, 460) // opp 30 Yard Line
-ctx.fillRect (894, 20, 1, 460) // opp 20 Yard Line
-ctx.fillRect (992, 20, 1, 460) // opp 10 Yard Line
-ctx.fillRect (1090, 20, 1, 460) // goal line - Right End Zone
-
+    // Create Yardage Lines (Current field is 980 px wide by 460px tall, each line marked 98 pixels apart)
+    ctx.fillRect (110, 20, 1, 460) // goal line - Left End Zone
+    ctx.fillRect (208, 20, 1, 460) // 10 Yard Line
+    ctx.fillRect (306, 20, 1, 460) // 20 Yard Line
+    ctx.fillRect (404, 20, 1, 460) // 30 Yard Line
+    ctx.fillRect (502, 20, 1, 460) // 40 Yard Line
+    ctx.fillRect (600, 20, 1, 460) // 50 Yard Line
+    ctx.fillRect (698, 20, 1, 460) // opp 40 Yard Line
+    ctx.fillRect (796, 20, 1, 460) // opp 30 Yard Line
+    ctx.fillRect (894, 20, 1, 460) // opp 20 Yard Line
+    ctx.fillRect (992, 20, 1, 460) // opp 10 Yard Line
+    ctx.fillRect (1090, 20, 1, 460) // goal line - Right End Zone
+}
 
 
 //Create Classes
@@ -89,15 +90,78 @@ newPlayer.render()
 joBackson.render()
 
 
+const gameLoopInterval = setInterval(gameLoop, 60)
+// const upPress = setInterval(function (direction, value) {
+//     joBackson.y -= 20
+// }, 60)
+
+const sideScroll = setInterval(function () {
+    joBackson.x += 10
+}, 300)
+
+// sideScroll() 
+
 // Map Keys (function handleKeyPressEvent with switch)
+function handleKeyPressEvent(e) {
+    const speed = 20
+    switch(e.key) {
+        case "w":
+        case "ArrowUp":
+            joBackson.y -= speed
+            break
+        case "s":
+        case "ArrowDown":
+            joBackson.y += speed
+            break
+        case "a":
+        case "ArrowLeft":
+            joBackson.x -= speed
+            break
+        case "d":
+        case "ArrowRight":
+            joBackson.x += speed
+            break
+    }
+}
+function handleKeyReleaseEvent(e) {
+    const speed = 20
+    switch(e.key) {
+        case "w":
+        case "ArrowUp":
+            clearInterval(upPress)
+            break
+        case "s":
+        case "ArrowDown":
+            joBackson.y += speed
+            break
+        case "a":
+        case "ArrowLeft":
+            joBackson.x -= speed
+            break
+        case "d":
+        case "ArrowRight":
+            joBackson.x += speed
+            break
+    }
+}
+
+
+document.addEventListener("keydown", handleKeyPressEvent)
 
 // Game Loop
+function gameLoop () {
     // clear off render
+    ctx.clearRect(0,0, canvas.width, canvas.height)
     // check for collision
         // tackle
         // touchdown
     // check game conditions
     // do all of the rendering
+    newPlayer.render()
+    joBackson.render()
+    renderField ()
+    // console.log("loop")-
+}
 
 // Console log for testing
 console.log(ctx)
